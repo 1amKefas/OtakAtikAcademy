2c339f98c6ab9506f3f8e1be564793c8a3bbdca6
kefas hutabarat <kefaskefas10@gmail.com>
2025-11-25 13:28:53 +0700

    Fix trusted proxies for Vercel HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'auth' => Illuminate\Auth\Middleware\Authenticate::class,
             'auth.basic' => Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
