69311f0e4e2ebb741f3809875ec7c4c3c23713df
kefas hutabarat <kefaskefas10@gmail.com>
2025-11-25 13:20:27 +0700

    In Production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         // Custom route macro for instructor routes
         Route::macro('instructor', function ($prefix = 'instructor') {
             return Route::prefix($prefix)
