52ba1368c2a2e3599c7c8a7961df24910f74734d
kefas hutabarat <kefaskefas10@gmail.com>
2025-11-29 16:59:32 +0700

    Logo Di title atas browser

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'OtakAtik')</title>
 
+    <link rel="icon" href="{{ asset('images/brain.png') }}" type="image/png">
+
     <!-- App assets (loaded via Vite) -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
