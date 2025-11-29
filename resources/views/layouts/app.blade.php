934479b59a009ff8fe32c478b45b28b55ad474f9
kefas hutabarat <kefaskefas10@gmail.com>
2025-11-29 17:19:32 +0700

    Dropdown Animation biar gk ngeleg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,8 @@
 
     <!-- FontAwesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
+    <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> @stack('head')
     @stack('head')
 </head>
 <body class="bg-gray-50 font-sans leading-normal tracking-normal">
