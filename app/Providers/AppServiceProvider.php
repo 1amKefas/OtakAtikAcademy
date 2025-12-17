6009fb899019ec08496f98ba34355553ae1111a4
kefas hutabarat <kefaskefas10@gmail.com>
2025-12-17 19:38:39 +0700

    N

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'local') {
+        // Cek jika environment adalah 'production' (settingan Fly.io)
+        if ($this->app->environment('production')) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
 
